fd4072f8251d17e249653cf2f99ad2a54d2c3662
Miguel Santos <miguel.g.santos@hotmail.com>
2015-12-23 09:12:33 +0100

    Initial Commit

---
+++ HideApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HideApp
+//
+//  Created by Miguel dos Santos on 23/12/2015.
+//  Copyright © 2015 Azool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
